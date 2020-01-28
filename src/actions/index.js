94f0af1eb0aed0e19eaa4fb9548434f3e66289b3
andrewssierra <andrewssierra@yahoo.com>
2020-01-27 21:42:07 -0800

    working on redux

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,10 +1,9 @@
 import jsonPlaceholder from '../apis/jsonPlaceHolder';
 
-export const fetchPosts = async () => {
+export const fetchPosts = () => async dispatch => {
     const response = await jsonPlaceholder.get('/posts');
-
-    return {
-        type: 'FETCH_POST',
+    dispatch({
+        type: 'FETCH_POSTS',
         payload: response
-    };
+    });
 };
